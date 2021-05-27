251500d1dcd2dfd1f15c210066a873fc6a39fbf1
vzavaliak@ukr.net <vzavaliak@ukr.net>
2021-05-27 15:51:03 +0300

    home work 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import {HttpClientModule} from "@angular/common/http";
+import { UserComponent } from './components/user/user.component';
+import { PostComponent } from './components/post/post.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    PostComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
